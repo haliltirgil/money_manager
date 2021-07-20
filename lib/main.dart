98468ee5f646a0286141d68017e0e97ee59b0054
haliltirgil <tirgil18@gmail.com>
2021-07-20 21:50:00 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:money_flow/theme.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: AppColors.greyColor,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("A/V Takibi"),
+      ),
+      body: Container(
+        color: AppColors.blackColor,
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors();
+
+  static const greyColor = const Color(0xFF2e2823);
+  static const greenColor = const Color(0xFF36A40F);
+  static const redColor = const Color(0xFFC60707);
+  static const blackColor = const Color(0xFF000000);
+  static const whiteColor = const Color(0xFFFFFFFF);
+}
